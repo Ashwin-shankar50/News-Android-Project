68e28dd79b6f19682906e572fdb6ab3de69dda13
Ashwin-shankar50 <ashwinshankar50@gmail.com>
2019-03-24 16:41:53 +0530

    WebView Included

---
+++ app/src/main/java/com/ashwin/application/newsapplication/Main2Activity.java
@@ -0,0 +1,27 @@
+package com.ashwin.application.newsapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Main2Activity extends AppCompatActivity {
+    private WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("url");
+        webview =(WebView)findViewById(R.id.webView);
+
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl(url);
+
+    }
+}
--- app/src/main/java/com/ashwin/application/newsapplication/MainActivity.java
+++ app/src/main/java/com/ashwin/application/newsapplication/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -34,30 +36,33 @@ public class MainActivity extends AppCompatActivity {
     static final String KEY_DESCRIPTION = "description";
     static final String KEY_URL = "url";
     static final String KEY_URLTOIMAGE = "urlToImage";
+    SwipeRefreshLayout pullToRefresh;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        pullToRefresh = (SwipeRefreshLayout) findViewById(R.id.pullToRefresh);
         listNews = (ListView) findViewById(R.id.listNews);
-
-
-
         DownloadNews newsTask = new DownloadNews();
         newsTask.execute();
+        pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                DownloadNews newsTask = new DownloadNews();
+                newsTask.execute();
+                pullToRefresh.setRefreshing(false);
+            }
 
-
-
+        });
 
     }
-
 
     class DownloadNews extends AsyncTask<String, Void, String> {
         @Override
 
         protected String doInBackground(String... args) {
             String line = "";
-
 
             try {
                 URL myapi = new URL("https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=0baa683f251d461b9f4e8f44ab6e72bb");
@@ -71,9 +76,11 @@ public class MainActivity extends AppCompatActivity {
                     line = line + data;
                     //   Log.d("TEST STRING",data);
                 }
-            } catch (MalformedURLException e) {
+            }
+            catch (MalformedURLException e) {
 
-            } catch (IOException e) {
+            }
+            catch (IOException e) {
 
             }
 
@@ -83,43 +90,49 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String line) {
 
             try {
-                JSONObject jsonResponse = new JSONObject(line);
-                JSONArray jsonArray = jsonResponse.optJSONArray("articles");
+                JSONObject jo1 = new JSONObject(line);
+                JSONArray ja1 = jo1.optJSONArray("articles");
 
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                for (int i = 0; i < ja1.length(); i++) {
+
+                    JSONObject jo2 = ja1.getJSONObject(i);
                     HashMap<String, String> map = new HashMap<String, String>();
-                    map.put(KEY_TITLE, jsonObject.optString(KEY_TITLE).toString());
-                    map.put(KEY_DESCRIPTION, jsonObject.optString(KEY_DESCRIPTION).toString());
-                    map.put(KEY_URL, jsonObject.optString(KEY_URL).toString());
-                    map.put(KEY_URLTOIMAGE, jsonObject.optString(KEY_URLTOIMAGE).toString());
-
+                    map.put(KEY_TITLE, jo2.optString(KEY_TITLE).toString());
+                    map.put(KEY_DESCRIPTION, jo2.optString(KEY_DESCRIPTION).toString());
+                    map.put(KEY_URL, jo2.optString(KEY_URL).toString());
+                    map.put(KEY_URLTOIMAGE, jo2.optString(KEY_URLTOIMAGE).toString());
                     dataList.add(map);
+                      //Log.d("TEST STRING", String.valueOf(map));
                 }
-            } catch (JSONException e) {
+            }
+
+            catch (JSONException e) {
             }
 
             ListNewsAdapter adapter = new ListNewsAdapter(MainActivity.this, dataList);
             listNews.setAdapter(adapter);
             listNews.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                public void onItemClick(AdapterView<?> parent, View view,
-                                        int position, long id) {
-                    String ne = dataList.get(+position).get(KEY_URL);
-                    Intent browserIntent = new Intent(
+
+                public void onItemClick(AdapterView<?> parent, View view,int position, long id)
+                {
+                    String ne = dataList.get(position).get(KEY_URL);
+                   /* Intent browserIntent = new Intent(
                             Intent.ACTION_VIEW,
                             Uri.parse(ne));
-                    startActivity(browserIntent);
+                    startActivity(browserIntent);*/
+                    Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                    intent.putExtra("url",ne);
+
+                    startActivity(intent);
+
 
                 }
-            });
 
+            });
 
         }
 
 
-
     }
-
-
 
 }
